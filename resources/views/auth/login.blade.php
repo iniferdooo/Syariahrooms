e4a2af7c7178ca5852d4d16a93f53fc76e533e33
dennisricco <99968743+dennisricco@users.noreply.github.com>
2024-03-20 14:48:00 +0700

    oioioi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,9 +9,7 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <title>Login</title>
 </head>
-
 <body class="bg-gray-100 w-full">
-<<<<<<< HEAD
   <section class="min-h-screen flex flex-col items-center justify-center">
     <div class="z-50 bg-gray-100 flex flex-col sm:flex-row rounded-2xl shadow-lg max-w-3xl p-5">
       <div class="sm:w-1/2 sm:block hidden ">
@@ -20,7 +18,6 @@
       <div class="w-full sm:w-1/2 pl-7 pr-7">
         <h2 class="font-bold text-black text-2xl font-sans">Welcome Back</h2>
         <p class="text-[#63B3A8] mt-1 font-sans">Login your account</p>
-
         <form class="flex flex-col gap-2" form action="/loginproses" method="post">
           @csrf
           <div class="flex flex-col sm:flex-row w-[330px] bg-[#DCDCDC] mt-3 rounded-lg gap-2">
@@ -31,45 +28,7 @@
               class="bi bi-envelope absolute translate-x-2 translate-y-3 " viewBox="0 0 16 16">
               <path
                 d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-            </svg>
-=======
-    <section class="min-h-screen flex flex-col items-center justify-center">
-        <div class="z-50 bg-gray-100 flex flex-col sm:flex-row rounded-2xl shadow-lg max-w-3xl p-5">
-          <div class="sm:w-1/2 sm:block hidden ">
-            <img class="h-full rounded-2xl" src="../../assets/bed.jpg" alt="">
-        </div>
-          <div class="w-full sm:w-1/2 pl-7 pr-7">
-            <h2 class="font-bold text-black text-2xl font-sans">Welcome Back</h2>
-              <p class="text-[#63B3A8] mt-1 font-sans">Login your account</p>
-              <form class="flex flex-col gap-2"action="login" method="POST">
-                <div class="flex flex-col sm:flex-row w-[330px] bg-[#DCDCDC] mt-3 rounded-lg gap-2">
-                  <input class="bg-[#DCDCDC] p-2 ml-7 border-l-2 border-l-[#DCDCDC] rounded-lg text-black font-sans sm:w-[304px]" type="email" name="email" id="email" placeholder="Email" required>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope absolute translate-x-2 translate-y-3 " viewBox="0 0 16 16">
-                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-                  </svg>
-                </div>
-                <div class="flex flex-col sm:flex-row w-[330px] bg-[#DCDCDC] rounded-lg gap-2 mt-2">
-                  <input class="bg-[#DCDCDC] p-2 ml-6 border-l-2 border-l-[#DCDCDC] rounded-lg text-black font-sans sm:w-[304px]" type="password" name="password" id="password" placeholder="Password" required>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key absolute translate-x-2 translate-y-3" viewBox="0 0 16 16">
-                    <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
-                    <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-                  </svg>
-                </div>
-                <div id="checkPassword" class="font-blod text-[#B22222] mt-2 font-sans">Wrong pasword. Please try again.</div>
-                <div class="mt-3 flex justify-between items-center">
-                    <div>
-                        <input type="checkbox" name="remember me" id="rememberme">
-                        <label for="" class="font-sans">Remember me</label> 
-                    </div>
-                    <div>
-                        <a href="#" class="text-black font-sans">Forgot Password</a>
-                    </div>
-                </div>
-                
-                <button class="bg-[#63B3A8] text-white p-2 rounded-md font-sans">Login</button>
-              </form>
-              <p class="mt-9 text-center">Don't have an account? <a class="text-[#63B3A8]" href="register">Register</a></p>
->>>>>>> 54e20ccf1f0165a87f04df7f3d7d09de8d110227
+            </svg> 
           </div>
           <div class="flex flex-col sm:flex-row w-[330px] bg-[#DCDCDC] rounded-lg gap-2">
             <input
@@ -92,7 +51,6 @@
               <a href="#" class="text-black font-sans">Forgot Password</a>
             </div>
           </div>
-
           <button class="bg-[#63B3A8] text-white p-2 rounded-md font-sans">Login</button>
         </form>
         <p class="mt-9 text-center">Don't have an account? <a class="text-[#63B3A8]" href="register">Register</a></p>
